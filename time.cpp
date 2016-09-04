7a1265117669168d2e6b8838268e5feef1bfed04
JaumNetto <joaom_oliveira@outlook.com>
2016-09-04 17:33:22 -0300

    Add files via upload

---
+++ time.cpp
@@ -0,0 +1,69 @@
+/*
+	Name: Calendario.cpp
+	Author: João Manoel de Oliveira Neto / Davi Medeiros
+	Date: 04/09/16 15:51
+	Description: Programa para produzir um calendario
+*/
+ #include <stdio.h>
+
+ 
+ 
+ main()
+ {
+ 	
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  /* int main(void)
+   {
+       int d, m, a, res, x;
+       scanf("%d", &d); //dia
+       scanf("%d", &m); //mes
+        scanf("%d", &a); //ano
+        if (m > 2)
+    {
+           m = m - 2;
+           x = 8;
+       }
+       else
+       {
+           a = a - 1;
+           x = 13;
+       }
+       res = (x + d + 2 * m + ((7 * m) / 12) + a % 7 +
+           a / 4 % 7 - (3 * (1 + a / 100)) / 4 % 7) % 7;
+       if (res == 0) printf("Sabado");
+       else
+          if (res == 1) printf("Domingo");
+          else
+              if (res == 2) printf("Segunda");
+             else
+                   if (res == 3) printf("Terca");
+                   else
+                      if (res == 4) printf("Quarta");
+                      else
+                           if (res == 5) printf("Quinta");
+                           else
+                              if (res == 6) printf("Sexta");
+      return 0;
+   }*/
